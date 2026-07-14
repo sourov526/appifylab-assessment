2f8f4f0a9e4cfecb3ce1f4bf1906b01d4944048d
sourov <sourovsh533@gmail.com>
2026-07-14 15:52:25 +0600

    clean post like unlike

--- client/components/feed/PostCard.tsx
+++ client/components/feed/PostCard.tsx
@@ -179,33 +179,7 @@ export function PostCard({ post, onPostUpdate }: PostCardProps) {
             onClick={handlePostLikeToggle}
           >
             <span className="_feed_inner_timeline_reaction_link">
-              <span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="19"
-                  height="19"
-                  fill="none"
-                  viewBox="0 0 19 19"
-                >
-                  <path
-                    fill="#FFCC4D"
-                    d="M9.5 19a9.5 9.5 0 100-19 9.5 9.5 0 000 19z"
-                  />
-                  <path
-                    fill="#664500"
-                    d="M9.5 11.083c-1.912 0-3.181-.222-4.75-.527-.358-.07-1.056 0-1.056 1.055 0 2.111 2.425 4.75 5.806 4.75 3.38 0 5.805-2.639 5.805-4.75 0-1.055-.697-1.125-1.055-1.055-1.57.305-2.838.527-4.75.527z"
-                  />
-                  <path
-                    fill="#fff"
-                    d="M4.75 11.611s1.583.528 4.75.528 4.75-.528 4.75-.528-1.056 2.111-4.75 2.111-4.75-2.11-4.75-2.11z"
-                  />
-                  <path
-                    fill="#664500"
-                    d="M6.333 8.972c.729 0 1.32-.827 1.32-1.847s-.591-1.847-1.32-1.847c-.729 0-1.32.827-1.32 1.847s.591 1.847 1.32 1.847zM12.667 8.972c.729 0 1.32-.827 1.32-1.847s-.591-1.847-1.32-1.847c-.729 0-1.32.827-1.32 1.847s.591 1.847 1.32 1.847z"
-                  />
-                </svg>
-                {post.likedByMe ? "Haha" : "Like"}
-              </span>
+              <span>{post.likedByMe ? "Unlike" : <>Like</>}</span>
             </span>
           </button>
           <button
