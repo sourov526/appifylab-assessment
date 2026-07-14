c4dccfdc8e8197265df423baf522907bac48f9af
sourov <sourovsh533@gmail.com>
2026-07-14 16:42:01 +0600

    add a comment send icon as button

--- client/components/feed/PostCard.tsx
+++ client/components/feed/PostCard.tsx
@@ -272,9 +272,9 @@ export function PostCard({ currentUser, post, onPostUpdate }: PostCardProps) {
               </div>
               <div className="_feed_inner_comment_box_icon">
                 <button
-                  type="submit"
+                  type="button"
                   className="_feed_inner_comment_box_icon_btn"
-                  disabled={isSubmittingComment}
+                  aria-label="Voice input"
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -309,6 +309,28 @@ export function PostCard({ currentUser, post, onPostUpdate }: PostCardProps) {
                       fillOpacity=".46"
                       fillRule="evenodd"
                       d="M10.867 1.333c2.257 0 3.774 1.581 3.774 3.933v5.435c0 2.352-1.517 3.932-3.774 3.932H5.101c-2.254 0-3.767-1.58-3.767-3.932V5.266c0-2.352 1.513-3.933 3.767-3.933h5.766zm0 1H5.101c-1.681 0-2.767 1.152-2.767 2.933v5.435c0 1.782 1.086 2.932 2.767 2.932h5.766c1.685 0 2.774-1.15 2.774-2.932V5.266c0-1.781-1.089-2.933-2.774-2.933zm.426 5.733l.017.015.013.013.009.008.037.037c.12.12.453.46 1.443 1.477a.5.5 0 11-.716.697S10.73 8.91 10.633 8.816a.614.614 0 00-.433-.118.622.622 0 00-.421.225c-1.55 1.88-1.568 1.897-1.594 1.922a1.456 1.456 0 01-2.057-.021s-.62-.63-.63-.642c-.155-.143-.43-.134-.594.04l-1.02 1.076a.498.498 0 01-.707.018.499.499 0 01-.018-.706l1.018-1.075c.54-.573 1.45-.6 2.025-.06l.639.647c.178.18.467.184.646.008l1.519-1.843a1.618 1.618 0 011.098-.584c.433-.038.854.088 1.19.363zM5.706 4.42c.921 0 1.67.75 1.67 1.67 0 .92-.75 1.67-1.67 1.67-.92 0-1.67-.75-1.67-1.67 0-.921.75-1.67 1.67-1.67zm0 1a.67.67 0 10.001 1.34.67.67 0 00-.002-1.34z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </button>
+                <button
+                  type="submit"
+                  className="_feed_inner_comment_box_icon_btn"
+                  aria-label="Send comment"
+                  disabled={isSubmittingComment}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="none"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fill="#000"
+                      fillOpacity=".46"
+                      fillRule="evenodd"
+                      d="M14.43 1.57a.75.75 0 00-.77-.18l-11 4a.75.75 0 00.05 1.43l4.47 1.34 1.34 4.47a.75.75 0 001.43.05l4-11a.75.75 0 00-.18-.77zm-5.06 9.35l-.94-3.14a.75.75 0 00-.5-.5l-3.14-.94 7.4-2.69-2.69 7.27z"
                       clipRule="evenodd"
                     />
                   </svg>
